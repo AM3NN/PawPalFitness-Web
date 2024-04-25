93350d050dab4d6805e10043855639c1238c2a62
rouaadridii <157540603+rouaadridii@users.noreply.github.com>
2024-04-25 10:15:09 +0100

    validation1

--- src/Entity/Animal.php
+++ src/Entity/Animal.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\AnimalRepository;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass:AnimalRepository::class)]
 class Animal
--- src/Form/AnimalType.php
+++ src/Form/AnimalType.php
@@ -18,37 +18,20 @@ class AnimalType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', null, [
-                'constraints' => [
-                    new NotBlank(['message' => 'Vous devez mettre le nom de votre animal!!!']),
-                ],
-            ])
+            ->add('nom')
             ->add('age', null, [
                 'constraints' => [
-                    new NotBlank(['message' => 'Vous devez mettre l\'age!!!']),
                     new Type(['type' => 'integer', 'message' => 'Âge doit être un nombre entier']),
                     new Range(['min' => 1, 'minMessage' => 'Âge doit être supérieur à 0']),
                 ],
             ])
             ->add('categorie', ChoiceType::class, [
                 'choices' => ['Chien' => 'Chien', 'Chat' => 'Chat'],
-                'constraints' => [
-                    new NotBlank(['message' => 'Vous devez choisir la categorie!!!']),
-                ],
             ])
-            ->add('type', null, [
-                'constraints' => [
-                    new NotBlank(['message' => 'Vous devez mettre le type!!!']),
-                ],
-            ])
-            ->add('details', TextareaType::class, [
-                'constraints' => [
-                    new NotBlank(['message' => 'Vous devez mettre les details!!!']),
-                ],
-            ])
+            ->add('type')
+            ->add('details', TextareaType::class)
             ->add('poids', null, [
                 'constraints' => [
-                    new NotBlank(['message' => 'Vous devez mettre le poids!!!']),
                     new Type(['type' => 'float', 'message' => 'Le poids doit être un nombre décimal']),
                     new Range(['min' => 0.1, 'minMessage' => 'Le poids doit être supérieur à 0']),
                 ],
